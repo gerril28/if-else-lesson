7928b8c54b11ee27e0bb15b9b7fa76aeaf7590a7
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-26 09:14:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+/**
+ * did from workspace called "notes: If else conditons" we did the first 2
+ */
+/**
+ * do assigment in the workspace called "If Else: Assignment 3"
+ */
 input.onButtonPressed(Button.A, function () {
     if (rain == true) {
         basic.showString("Raincoat")
